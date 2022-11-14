7b8822af33683bb9253b61bd176aa7785ea5a00d
Ryan Borger <116569719+Ryanstyles33@users.noreply.github.com>
2022-11-14 22:12:25 +0000

    7

--- files.js
+++ files.js
@@ -20,9 +20,28 @@ fs.readFile('./docs/blog1.txt', (err, data) => {
 // });
 
 //directories
+if (!fs.existsSync('./assets')) {
  fs.mkdir('./assets', err => {
     if (err) {
       console.log(err);
     }
     console.log('folder created');
   })
+}  else {
+  fs.rmdir('./assets', err => {
+    if (err) {
+      console.log(err);
+    }
+    console.log('folder deleted');
+  })
+}
+
+// deleting files
+if (fs.existsSync('./docs/deleteme.txt')) {
+ fs.unlink('./docs/deleteme.txt', err => {
+    if (err) {
+      console.log(err)
+    }
+    console.log('file deleted');
+  })
+}

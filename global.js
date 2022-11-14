6d49deb1c2adb8216667510b0d973e1c3bedfe7c
Ryan Borger <116569719+Ryanstyles33@users.noreply.github.com>
2022-11-14 21:09:51 +0000

    interval

--- global.js
+++ global.js
@@ -2,8 +2,10 @@
 
 //console.log(global);
 
-console.log(global);
-
 setTimeout(()=> {
   console.log('in the timeout');
 }, 30000);
+
+const int = setInterval(() => {
+  console.log ('in the interval')
+}, 1000);
